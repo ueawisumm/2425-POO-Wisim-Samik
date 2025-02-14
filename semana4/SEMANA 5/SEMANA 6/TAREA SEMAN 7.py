bf998a5df2373df5c310fcad7c7a667e4989e9bd
sc.wisumm@uea.edu.ec <jeykam2017@hotmail.com>
2025-02-14 05:20:24 -0500

    semana 9

--- semana4/SEMANA 5/SEMANA 6/TAREA SEMAN 7.py
+++ semana4/SEMANA 5/SEMANA 6/TAREA SEMAN 7.py
@@ -1,9 +1,27 @@
-import self  #metodo constructor y destructor
-  class veiculo
-      def__init__vehiculo:(self, marca, modelo):
 
-      self.self marca = marac
-      self. modelo =modelo
+class persona:
+    # metodo constructor
+    def __init__(self, nombre, edad,):
+        self.nombre = nombre
+        self.edad = edad
+
+class persona(genero):
+    def __init__(self, nombre, edad):
+    super __init__ (nombre, edad):
+      self.edad = edad
+        def mostrarinfo(self):
+            print(f" la personajes", self edad 20)
+            print(f"selecciona la edad es 20:", self.edad)
+            print(f"la edad seleccionada es; 20",self. edad)
+
+
+
+
+
+
+
+
+
 
 
 
